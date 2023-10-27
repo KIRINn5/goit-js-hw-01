870c380a860a45581084a8dbdbb8262f2f287923
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2023-10-27 22:42:11 +0300

    fourth commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
 "strict mode"
 
 function makeTransaction(quantity, pricePerDroid) {
-    const product = `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
-    return quantity * pricePerDroid;
+    const profuct = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
