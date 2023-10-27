c8887045242266c663af9dc642112b069f13c61d
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2023-10-27 15:22:54 +0300

    third commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
 "strict mode"
 
 function getShippingMessage(country, price, deliveryFee) {
-    const delivery = 'Shipping to ${country} will cost ${price+deliveryFee} credits'; 
-    return country, price + deliveryFee;
+    const delivery = price + deliveryFee; 
+    return `Shipping to ${country} will cost ${price+deliveryFee} credits`;
 }
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
